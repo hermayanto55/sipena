118cdb54a61456866cdace671c695b902800a1de
hermayanto55 <hermayanto55@gmail.com>
2026-08-01 07:13:03 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+/**
+ * Isi URL deployment Google Apps Script yang berakhiran /exec.
+ * Contoh: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
+ */
+window.SIPENA_CONFIG = Object.freeze({
+  GAS_EXEC_URL: 'PASTE_URL_GOOGLE_APPS_SCRIPT_EXEC_DI_SINI',
+  API_TIMEOUT_MS: 45000
+});
