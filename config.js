2954709af9ea36c8ac35e6c7f92877eea7810d3b
hermayanto55 <hermayanto55@gmail.com>
2026-08-01 07:06:00 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Isi URL deployment Google Apps Script yang berakhiran /exec.
- * Contoh: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
- */
-window.SIPENA_CONFIG = Object.freeze({
-  GAS_EXEC_URL: 'https://script.google.com/macros/s/AKfycbxbycmwq4KtbpYCMrLeuOOpErhY-skC28FIotPwtJzWlQXA6vi4fS3vmse-j3mTpr82/exec',
-  API_TIMEOUT_MS: 45000
-});
